d1f6e1edd0f175c0e53e3e1a007ad0729511d90b
vaibhav340 <33030703+vaibhav340@users.noreply.github.com>
2022-03-04 00:13:04 +0530

    Update DateTimeBlock.php

--- datetime_block/src/Plugin/Block/DateTimeBlock.php
+++ datetime_block/src/Plugin/Block/DateTimeBlock.php
@@ -21,19 +21,19 @@ class DateTimeBlock extends BlockBase {
    */
   public function build() {
 	
-	$country = \Drupal::config('datetime_block.settings')->get('country_name');
+ 	$country = \Drupal::config('datetime_block.settings')->get('country_name');
 	$city = \Drupal::config('datetime_block.settings')->get('city_name');
 	$timezone = \Drupal::config('datetime_block.settings')->get('timezone');
 	// Date Time Service Used to get date time based on timezone
 	$datetime =\Drupal::service('datetime_block.get_datetime')->getDate($timezone);
 	
 	return [
-      '#theme' => 'datetime_block',
-      '#data' => ['Country' => $country, 'City' => $city, 'Date and Time' => $datetime],
-	  '#cache' => [
+      	  '#theme' => 'datetime_block',
+          '#data' => ['Country' => $country, 'City' => $city, 'Date and Time' => $datetime],
+          '#cache' => [
             'max-age' => 0,
-        ],	
-    ];
+          ],	
+        ];
   }
 
 }
