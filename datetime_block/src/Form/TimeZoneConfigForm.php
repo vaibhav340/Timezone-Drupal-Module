a556067b2f06cc027630e1b96c52c75fb7342c49
vaibhav340 <33030703+vaibhav340@users.noreply.github.com>
2022-03-04 00:11:01 +0530

    Update TimeZoneConfigForm.php

--- datetime_block/src/Form/TimeZoneConfigForm.php
+++ datetime_block/src/Form/TimeZoneConfigForm.php
@@ -36,26 +36,26 @@ class TimeZoneConfigForm extends ConfigFormBase {
       '#title' => $this->t('Country'),
       '#default_value' => $config->get('country_name'),
     ];
-	$form['city_name'] = [
+    $form['city_name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('City'),
       '#default_value' => $config->get('city_name'),
     ];
-	$form['timezone'] = [
+    $form['timezone'] = [
       '#type' => 'select',
       '#title' => $this->t('Timezone'),
-	  '#options' => [
-		'America/Chicago' => 'America/Chicago',
-		'America/New_York' => 'America/New_York',
-		'Asia/Tokyo' => 'Asia/Tokyo',
-		'Asia/Dubai' => 'Asia/Dubai',
-		'Asia/Kolkata' => 'Asia/Kolkata',
-		'Europe/Amsterdam' => 'Europe/Amsterdam',
-		'Europe/Oslo' => 'Europe/Oslo',
-		'Europe/London' => 'Europe/London',
-		],
+      '#options' => [
+	'America/Chicago' => 'America/Chicago',
+	'America/New_York' => 'America/New_York',
+	'Asia/Tokyo' => 'Asia/Tokyo',
+	'Asia/Dubai' => 'Asia/Dubai',
+	'Asia/Kolkata' => 'Asia/Kolkata',
+	'Europe/Amsterdam' => 'Europe/Amsterdam',
+	'Europe/Oslo' => 'Europe/Oslo',
+	'Europe/London' => 'Europe/London',
+	],
       '#default_value' => $config->get('timezone'),
-	  ];
+    ];
 	
     return parent::buildForm($form, $form_state);
   }
@@ -65,10 +65,10 @@ class TimeZoneConfigForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('datetime_block.settings')
-	  ->set('country_name', $form_state->getValue('country_name'))
-      ->set('city_name', $form_state->getValue('city_name'))
-	  ->set('timezone', $form_state->getValue('timezone'))
-      ->save();
+       ->set('country_name', $form_state->getValue('country_name'))
+       ->set('city_name', $form_state->getValue('city_name'))
+       ->set('timezone', $form_state->getValue('timezone'))
+       ->save();
     parent::submitForm($form, $form_state);
   }
 
